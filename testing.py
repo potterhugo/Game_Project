9c0d23d498e3adf51998ed4c2077fe49873b6978
Nick <nicholas.j.potter@gmail.com>
2017-07-08 14:43:44 +1000

    Score, game over and chance to replay!

--- testing.py
+++ testing.py
@@ -10,18 +10,21 @@ playSurface = pygame.display.set_mode((640,480))
 pygame.display.set_caption('CHASE THE DOT!!!')
 RESETEVENT = pygame.USEREVENT + 1
 
-
+pygame.font.init() 
+myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
 def d(p1, p2):
     return math.sqrt((p1[0]-p2[0])**2 + (p1[1]-p2[1])**2)
 
+INIT_TIME = 2200
+
 flag = True
+PLAYING = True
 score = 0
+TIME = INIT_TIME
 while True:
-    tr = pygame.key.get_pressed()
-    #if event.key == K_ESCAPE:
-    #    sys.exit()
-    for event in pygame.event.get():
+    if PLAYING:
+        for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 print('Your score = %d' % score)
                 pygame.quit()
@@ -33,8 +36,8 @@ while True:
                     pygame.quit()
             elif event.type == pygame.MOUSEBUTTONDOWN:
                 print(event.pos)
-                score += 1
                 if d(event.pos, circle_pos) <= 30:
+                    score += 1
                     pygame.draw.circle(playSurface,
                                        pygame.Color(0,255,0),
                                        circle_pos,
@@ -42,7 +45,6 @@ while True:
                     pygame.display.flip()
                     pygame.time.wait(150)
                 else:
-                    score -= 1
                     pygame.draw.circle(playSurface,
                                        pygame.Color(255,0,0),
                                        circle_pos,
@@ -50,6 +52,8 @@ while True:
                     pygame.display.flip()
                     pygame.time.wait(150)
                 flag = True
+                TIME = int(TIME*0.9)
+                print(TIME)
             elif event.type == RESETEVENT:
                 print('asdf')
                 pygame.draw.circle(playSurface,
@@ -58,19 +62,42 @@ while True:
                                    30,0)
                 pygame.display.flip()
                 pygame.time.wait(150)
-                flag = True
-    if flag:
-        playSurface.fill(pygame.Color(0,0,0)) # Clear screen
+                PLAYING = False
+                break
+        if flag:
+            playSurface.fill(pygame.Color(0,0,0)) # Clear screen
 
-        circle_pos = (random.randint(0,610)+15, random.randint(0,450)+15)
-        
-        pygame.draw.circle(playSurface,
-                           pygame.Color(255,255,255),
-                           circle_pos,
-                           30,0)
-        flag = False
-        pygame.time.set_timer(RESETEVENT, 1000)
-        
-        pygame.display.flip()                
-                
+            circle_pos = (random.randint(0,610)+15, random.randint(0,450)+15)
+
+            pygame.draw.circle(playSurface,
+                               pygame.Color(255,255,255),
+                               circle_pos,
+                               30,0)
+            flag = False
+            pygame.time.set_timer(RESETEVENT, TIME)
+
+            pygame.display.flip()
+    else:
+        playSurface.fill(pygame.Color(0,0,0))
+        text1 = myfont.render('GAME OVER - your score: %d' % score,
+                              False, (255,)*3)
+        text2 = myfont.render('Play again? Y/N',
+                              False, (255,)*3)
+        playSurface.blit(text1,(100,100))
+        playSurface.blit(text2,(100,150))
+        pygame.display.flip()
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                print(event.key)
+                print(pygame.key.get_pressed())
+                if event.key == 110:
+                    pygame.quit()
+                elif event.key == 121:
+                    PLAYING = True
+                    flag = True
+                    score = 0
+                    TIME = INIT_TIME
+                    break
+            
+
 
